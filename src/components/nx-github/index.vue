090722a6bf1ee38cbffecc503bb4833ad0046b72
wangxiaoyu <wangxiaoyu@wtipcc.com>
2020-02-19 23:49:25 +0800

    initial
    Change-Id: Id020eabb0c7b67024661dad11a9a8c3ef9bfab39

---
+++ src/components/nx-github/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <el-tooltip effect="dark" placement="bottom">
+      <div slot="content">
+        <a class="link" :href="url" target="_blank">消息通知</a>
+      </div>
+      <el-button class="btn-text can-hover" type="text" @click="handleClick">
+        <nx-icon name="bell" style="font-size: 20px"/>
+      </el-button>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+import nxIcon from '@/components/nx-icon'
+export default {
+  name: 'nx-github',
+  components: { nxIcon },
+  data() {
+    return {
+      url: 'https://github.com/jiechengshidai'
+    }
+  },
+  methods: {
+    handleClick() {
+      window.open(this.url)
+    }
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+.link {
+  color: #FFF;
+  text-decoration: none;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,138 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+// const _import = require('./_import_' + process.env.NODE_ENV)
+// in development-env not use lazy-loading, because lazy-loading too many pages will cause webpack hot update too slow. so only in production use lazy-loading;
+// detail: https://panjiachen.github.io/vue-element-admin-site/#/lazy-loading
+
+Vue.use(Router)
+
+/* Layout */
+import Layout from '../views/layout/Layout'
+
+/**
+* hidden: true                   if `hidden:true` will not show in the sidebar(default is false)
+* alwaysShow: true               if set true, will always show the root menu, whatever its child routes length
+*                                if not set alwaysShow, only more than one route under the children
+*                                it will becomes nested mode, otherwise not show the root menu
+* redirect: noredirect           if `redirect:noredirect` will no redirct in the breadcrumb
+* name:'router-name'             the name is used by <keep-alive> (must set!!!)
+* meta : {
+    title: 'title'               the name show in submenu and breadcrumb (recommend set)
+    icon: 'svg-name'             the icon show in the sidebar,
+  }
+**/
+export const constantRouterMap = [
+  // {
+  //   path: '/',
+  //   component: Layout,
+  //   redirect: '/login',
+  //   name: 'login',
+  //   hidden: true
+  // },
+  {
+    path: '',
+    component: Layout,
+    redirect: '/dashboard/dashboard'
+  },
+  { path: '/login', component: () => import('@/views/login'), name: '登录系统', hidden: true },
+  { path: '/404', component: () => import('@/views/errorPage/404'), hidden: true },
+  { path: '/401', component: () => import('@/views/errorPage/401'), hidden: true },
+
+  // 首页
+  {
+    path: '/dashboard',
+    component: Layout,
+    meta: { title: 'dashboard', icon: 'dashboard' },
+    children: [
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () => import('@/views/dashboard/dashboard'),
+        meta: { title: 'dashboard', icon: 'dashboard' }
+      }
+    ]
+  },
+
+  // 表格
+  {
+    path: '/table',
+    component: Layout,
+    redirect: '/table/complex-table',
+    name: 'table',
+    meta: {
+      title: 'Table',
+      icon: 'table'
+    },
+    children: [
+      {
+        path: 'complex-table',
+        name: 'complex-table',
+        component: () => import('@/views/table/complex-table'),
+        meta: { title: 'complexTable' }
+      },
+      {
+        path: 'TreeTable',
+        name: 'TreeTable',
+        component: () => import('@/views/table/tree-table/index'),
+        meta: { title: 'treeTable' }
+      }
+
+    ]
+  }
+  // 表单
+  // {
+  //   path: '/form',
+  //   component: Layout,
+  //   redirect: '/table/BaseForm',
+  //   name: 'form',
+  //   meta: {
+  //     title: '其他分类',
+  //     icon: 'form'
+  //   },
+  //   children: [
+  //     {
+  //       path: '子分类1',
+  //       name: 'BaseForm',
+  //       component: () => import('@/views/form/BaseForm'),
+  //       meta: { title: 'BaseForm' }
+  //     },
+  //     {
+  //       path: '子分类2',
+  //       name: 'VueEditor',
+  //       component: () => import('@/views/form/VueEditor'),
+  //       meta: { title: 'VueEditor' }
+  //     },
+  //     {
+  //       path: '子分类3',
+  //       name: 'Upload',
+  //       component: () => import('@/views/form/Upload'),
+  //       meta: { title: 'Upload' }
+  //     }
+  //   ]
+  // }
+
+]
+
+export default new Router({
+  // mode: 'history', //后端支持可开
+  scrollBehavior: () => ({ y: 0 }),
+  routes: constantRouterMap
+})
+export const asyncRouterMap = [
+
+  {
+    path: '/error',
+    component: Layout,
+    redirect: 'noredirect',
+    name: 'errorPages',
+    meta: {
+      title: 'errorPages',
+      icon: '404'
+    },
+    children: [
+      { path: '401', component: () => import('@/views/errorPage/401'), name: 'page401', meta: { title: 'page401', noCache: true }},
+      { path: '404', component: () => import('@/views/errorPage/404'), name: 'page404', meta: { title: 'page404', noCache: true }}
+    ]
+  },
+  { path: '*', redirect: '/404', hidden: true }]
