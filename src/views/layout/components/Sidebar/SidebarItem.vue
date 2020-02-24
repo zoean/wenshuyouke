42acb4a9dc0689e6bd6fd46db7b59f436a0dc68b
xiaoliyan <xiaoliyan@wtipcc.com>
2020-02-24 14:15:26 +0800

    fix sidebar item hover css
    Change-Id: I6c6ff065282a3cc238a6495e03aa86dc9df8dd79

--- src/views/layout/components/Sidebar/SidebarItem.vue
+++ src/views/layout/components/Sidebar/SidebarItem.vue
@@ -1,28 +1,46 @@
 <template>
   <div class="menu-wrapper">
-    <template v-for="item in routes" v-if="!item.hidden&&item.children">
+    <template v-for="item in routes"
+              v-if="!item.hidden&&item.children">
 
-      <router-link v-if="hasOneShowingChildren(item.children) && !item.children[0].children&&!item.alwaysShow" :to="item.path+'/'+item.children[0].path"
-        :key="item.children[0].name">
-        <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
-          <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>
-          <span v-if="item.children[0].meta&&item.children[0].meta.title" slot="title">{{generateTitle(item.children[0].meta.title)}}</span>
+      <router-link v-if="hasOneShowingChildren(item.children) && !item.children[0].children&&!item.alwaysShow"
+                   :to="item.path+'/'+item.children[0].path"
+                   :key="item.children[0].name">
+        <el-menu-item :index="item.path+'/'+item.children[0].path"
+                      :class="{'submenu-title-noDropdown':!isNest}">
+          <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon"
+                    :icon-class="item.children[0].meta.icon"></svg-icon>
+          <span v-if="item.children[0].meta&&item.children[0].meta.title"
+                slot="title">{{generateTitle(item.children[0].meta.title)}}</span>
         </el-menu-item>
       </router-link>
 
-      <el-submenu v-else :index="item.name||item.path" :key="item.name">
+      <el-submenu v-else
+                  :index="item.name||item.path"
+                  :key="item.name">
         <template slot="title">
-          <svg-icon v-if="item.meta&&item.meta.icon" :icon-class="item.meta.icon"></svg-icon>
-          <span v-if="item.meta&&item.meta.title" slot="title">{{generateTitle(item.meta.title)}}</span>
+          <svg-icon v-if="item.meta&&item.meta.icon"
+                    :icon-class="item.meta.icon"></svg-icon>
+          <span v-if="item.meta&&item.meta.title"
+                slot="title">{{generateTitle(item.meta.title)}}</span>
         </template>
 
-        <template v-for="child in item.children" v-if="!child.hidden">
-          <sidebar-item :is-nest="true" class="nest-menu" v-if="child.children&&child.children.length>0" :routes="[child]" :key="child.path"></sidebar-item>
+        <template v-for="child in item.children"
+                  v-if="!child.hidden">
+          <sidebar-item :is-nest="true"
+                        class="nest-menu"
+                        v-if="child.children&&child.children.length>0"
+                        :routes="[child]"
+                        :key="child.path"></sidebar-item>
 
-          <router-link v-else :to="item.path+'/'+child.path" :key="child.name">
+          <router-link v-else
+                       :to="item.path+'/'+child.path"
+                       :key="child.name">
             <el-menu-item :index="item.path+'/'+child.path">
-              <svg-icon v-if="child.meta&&child.meta.icon" :icon-class="child.meta.icon"></svg-icon>
-              <span v-if="child.meta&&child.meta.title" slot="title">{{generateTitle(child.meta.title)}}</span>
+              <svg-icon v-if="child.meta&&child.meta.icon"
+                        :icon-class="child.meta.icon"></svg-icon>
+              <span v-if="child.meta&&child.meta.title"
+                    slot="title">{{generateTitle(child.meta.title)}}</span>
             </el-menu-item>
           </router-link>
         </template>
@@ -31,7 +49,16 @@
     </template>
   </div>
 </template>
-
+<style rel="stylesheet/scss" lang="scss">
+.el-menu-item:hover {
+  outline: 0 !important;
+  color: #fff !important;
+}
+.el-menu-item.is-active {
+  color: #fff !important;
+  background: #409eff !important;
+}
+</style>
 <script>
 import { generateTitle } from '@/utils/i18n'
 export default {
@@ -47,7 +74,7 @@ export default {
   },
   methods: {
     generateTitle,
-    hasOneShowingChildren(children) {
+    hasOneShowingChildren (children) {
       const showingChildren = children.filter(item => {
         return !item.hidden
       })
@@ -57,7 +84,7 @@ export default {
       return false
     }
   },
-  created() {
+  created () {
     for (const o in this.routes) {
       const obj = this.routes[o]
       if (obj.path.indexOf('myiframe') >= 0) {
