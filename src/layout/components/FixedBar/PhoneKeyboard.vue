40850e12d1df0a7fc69c6a49d67b2e91bdc1b08e
xiaoliyan <xiaoliyan@wtipcc.com>
2020-02-25 21:27:22 +0800

    hoverBar for telphone number keyboard
    Change-Id: I499731a027f3ef5d2bc0d45b846af461801dd8cd

---
+++ src/layout/components/FixedBar/PhoneKeyboard.vue
@@ -0,0 +1,174 @@
+<template>
+  <div class="app-wrapper">
+    <div class="keybord-pop">
+      <el-row class="pop-title">
+        <el-col :span="22">
+          <i class="el-icon-phone"></i>
+          拨打电话
+        </el-col>
+        <el-col :span="2">
+          <i class="el-icon-close"
+             @click="closePop()"></i>
+        </el-col>
+      </el-row>
+      <el-tabs v-model="activeName">
+        <el-tab-pane label="拨号键盘"
+                     name="first">
+          <el-row class="input-telno">
+            <el-input placeholder="输入电话号码"
+                      v-model="telNumber"
+                      minlength="4"
+                      maxlength="12"
+                      show-word-limit
+                      autofocus="true"
+                      clearable
+                      type="tel"
+                      @input="verifyInput"></el-input>
+          </el-row>
+          <ul class="number-keybord">
+            <li v-for="(numGrop,index) in keyboardNum"
+                :key="index">
+              <p v-for="number in numGrop"
+                 :key="number"
+                 @click="addNumber(number)">{{number}}</p>
+            </li>
+          </ul>
+          <el-row>
+            <el-button class="call-handle"
+                       size="medium"
+                       type="primary"
+                       @click="callHandle"
+                       :disabled="callDisable">呼叫</el-button>
+          </el-row>
+        </el-tab-pane>
+        <el-tab-pane label="呼叫历史"
+                     name="second">呼叫历史</el-tab-pane>
+      </el-tabs>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "PhoneKeyboard",
+  props: {
+    toggleKeyboard: {//关闭父组件的拨号panel方法
+      type: Function,
+      default: null
+    }
+  },
+  data () {
+    return {
+      activeName: 'first',
+      telNumber: '',
+      keyboardNum: [[1, 2, 3], [4, 5, 6], [7, 8, 9], ['*', 0, '#']],
+      callDisable: true
+    };
+  },
+  methods: {
+    closePop () {
+      this.toggleKeyboard()//调用父组件关闭panel方法
+    },
+    addNumber (item) {
+      this.telNumber += item
+      console.log(this.telNumber)
+    },
+    callHandle () {
+      console.log(this.telNumber)
+    },
+    verifyInput () {
+      if (this.telNumber.length < 5) {
+        this.callDisable = true
+      } else if (this.telNumber.length > 12) {
+        this.telNumber = this.telNumber(0, 12)
+        this.callDisable = false
+      } else {
+        this.callDisable = false
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.keybord-pop {
+  border: 1px solid #ebebeb;
+  box-shadow: 0 2px 6px 0 rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  position: fixed;
+  bottom: 10px;
+  right: 110px;
+  width: 260px;
+  height: 375px;
+  background: #f2f2f2;
+}
+.el-tabs {
+  background: #fff;
+  padding: 2px 0 10px 0;
+}
+.el-tabs__header {
+  padding: 0 10px;
+}
+.input-telno {
+  background: #fff;
+}
+.pop-title {
+  padding: 10px;
+}
+.el-icon-close {
+  cursor: pointer;
+}
+.input-telno,
+.el-tabs__nav {
+  padding: 0 20px;
+}
+/* 禁用浏览器默认加减按钮 */
+/* 去除webkit中input的type="number"时出现的上下图标 */
+input::-webkit-outer-spin-button,
+input::-webkit-inner-spin-button {
+  -webkit-appearance: none !important;
+  margin: 0;
+}
+input[type="number"] {
+  -moz-appearance: textfield;
+}
+.number-keybord {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  list-style: none;
+  width: 100%;
+  flex-grow: 1;
+  padding: 0;
+  border-bottom: 1px solid #eee;
+}
+.number-keybord li {
+  display: flex;
+  justify-content: center;
+  flex-grow: 1;
+}
+.number-keybord li p:nth-child(1) {
+  border-left: 0;
+}
+.number-keybord li p {
+  border: 1px solid #eee;
+  border-right: 0;
+  border-bottom: 0;
+  flex: 1;
+  text-align: center;
+  margin: 0;
+  height: 40px;
+  line-height: 40px;
+  cursor: pointer;
+}
+.number-keybord li p:hover {
+  background: #eee;
+}
+.number-keybord li p:active {
+  background: #ebebeb;
+}
+.call-handle {
+  margin-left: 10%;
+  width: 80%;
+}
+/* .number-keybord{} */
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,57 +1,59 @@
 <template>
-  <div :class="classObj"
-       class="app-wrapper">
-    <div v-if="device==='mobile'&&sidebar.opened"
-         class="drawer-bg"
-         @click="handleClickOutside" />
+  <div :class="classObj" class="app-wrapper">
+    <div
+      v-if="device === 'mobile' && sidebar.opened"
+      class="drawer-bg"
+      @click="handleClickOutside"
+    />
     <sidebar class="sidebar-container" />
     <div class="main-container">
-      <div :class="{'fixed-header':fixedHeader}">
+      <div :class="{ 'fixed-header': fixedHeader }">
         <navbar />
       </div>
       <app-main />
     </div>
-
+    <FixedBar class="fixed-bar" />
   </div>
 </template>
 
 <script>
-import { Navbar, Sidebar, AppMain } from './components'
-import ResizeMixin from './mixin/ResizeHandler'
-
+import { Navbar, Sidebar, AppMain } from "./components";
+import FixedBar from "./components/FixedBar/index";
+import ResizeMixin from "./mixin/ResizeHandler";
 export default {
-  name: 'Layout',
+  name: "Layout",
   components: {
     Navbar,
     Sidebar,
-    AppMain
+    AppMain,
+    FixedBar
   },
   mixins: [ResizeMixin],
   computed: {
-    sidebar () {
-      return this.$store.state.app.sidebar
+    sidebar() {
+      return this.$store.state.app.sidebar;
     },
-    device () {
-      return this.$store.state.app.device
+    device() {
+      return this.$store.state.app.device;
     },
-    fixedHeader () {
-      return this.$store.state.settings.fixedHeader
+    fixedHeader() {
+      return this.$store.state.settings.fixedHeader;
     },
-    classObj () {
+    classObj() {
       return {
         hideSidebar: !this.sidebar.opened,
         openSidebar: this.sidebar.opened,
         withoutAnimation: this.sidebar.withoutAnimation,
-        mobile: this.device === 'mobile'
-      }
+        mobile: this.device === "mobile"
+      };
     }
   },
   methods: {
-    handleClickOutside () {
-      this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
+    handleClickOutside() {
+      this.$store.dispatch("app/closeSideBar", { withoutAnimation: false });
     }
   }
-}
+};
 </script>
 
 <style lang="scss" scoped>
